06877afa360b96f3efe1bb07f84087821fad2403
MuhammadZulfikarIkhsan <71245150+MuhammadZulfikarIkhsan@users.noreply.github.com>
2020-09-29 21:46:27 +0700

    Update TP01.java

--- src/main/java/org/d3ifcool/ima04/TP01.java
+++ src/main/java/org/d3ifcool/ima04/TP01.java
@@ -4,5 +4,13 @@ public class TP01 {
 
 	public static void main(final String[] args) {
 		// Kerjakan soalnya di sini
+	        Scanner zlf = new Scanner(System.in);
+	        int input = zlf.nextInt();
+	        if (input == 6){
+	                System.out.println("Enam");
+	        }
+	        else{
+	                System.out.println("Bukan enam");
+	        }
 	}
 }
